a2b36b78cfc6d07a6ad5ced137d9679a993ce554
Leo <leo@circuscompany.com>
2021-03-08 14:18:16 +0900

    slove - A2751

---
+++ Baekjoon/A2751/A2751.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace A2751
+{
+    class A2751
+    {
+
+        static void Run()
+        {
+            StringBuilder sb = new StringBuilder();
+            int input = int.Parse(Console.ReadLine());
+            int[] array = new int[input];
+
+            for (int i = 0; i < input; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sb.AppendLine($"{array[i]}");
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
