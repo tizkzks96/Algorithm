4325b6d22a32b041b6696483f3c4dd8d22c18758
choiho <tizkzks96@naver.com>
2021-03-11 15:35:47 +0900

    Solve - A10989 (StreamReader, StreamWriter 를 써야 속도가 빠름)

--- Baekjoon/A10989/A10989.cs
+++ Baekjoon/A10989/A10989.cs
@@ -9,41 +9,52 @@ namespace A10989
     {
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            sw.AutoFlush = true;
             StringBuilder sb = new StringBuilder();
             int[] countArray = new int[10000];
 
-            //int input = int.Parse(Console.ReadLine());
+            int input = int.Parse(sr.ReadLine());
 
-            int input = 10000000;
-            int[] testCase = GetTestCase();
+            //int input = 10000000;
+            //int[] testCase = GetTestCase(input);
 
             for (int i = 0; i < input; i++)
             {
-                var temp = testCase[i];
+                //var temp = testCase[i] - 1;
+                var temp = int.Parse(sr.ReadLine()) - 1;
 
                 countArray[temp]++;
             }
 
             for (int i = 0; i < countArray.Length; i++)
             {
+                sb.Clear();
                 if (countArray[i] == 0) continue;
                 for (int j = 0; j < countArray[i]; j++)
                 {
-                    sb.AppendLine($"{i}");
+                    sb.Append($"{i + 1}\n");
                 }
+                sw.Write(sb);
             }
 
-            //Console.WriteLine(sb);
+
+            if (true)
+            {
+
+            }
         }
 
-        static int[] GetTestCase()
+        static int[] GetTestCase(int input)
         {
             StringBuilder sb = new StringBuilder();
 
-            int[] result = new int[10000000];
+            int[] result = new int[input];
             Random r = new Random();
 
-            for (int i = 0; i < 10000000; i++)
+            for (int i = 0; i < input; i++)
             {
                 result[i] = r.Next(1, 10001);
                 sb.AppendLine($"{result[i]}");
