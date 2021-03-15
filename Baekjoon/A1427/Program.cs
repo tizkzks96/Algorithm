f3ef3f728ce7897a116ddfa420606f03a36f1adf
Leo <leo@circuscompany.com>
2021-03-15 13:32:20 +0900

    Solve - A1427 (카운팅 정렬보다 더 빠른 정렬이 있다. string에서 Find하는 등??)

---
+++ Baekjoon/A1427/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace A1427
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            Run(sr.ReadLine());
+            //TestCase(100);
+        }
+
+        static void Run(string inputCase)
+        {
+            StringBuilder sb = new StringBuilder();
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            sw.AutoFlush = true;
+            int[] valuesCount = new int[10];
+            for (int i = 0; i < inputCase.Length; i++)
+            {
+                valuesCount[inputCase[i] - 48]++;
+            }
+
+            for (int i = 9; i >= 0; i--)
+            {
+                for (int j = 0; j < valuesCount[i]; j++)
+                {
+                    sb.Append(i);
+                }
+            }
+            sw.WriteLine(sb);
+        }
+
+        static void TestCase(int testCount)
+        {
+            Random r = new Random();
+            for (int i = 0; i < testCount; i++)
+            {
+                Run(r.Next(0, 1000000000).ToString());
+            }
+        }
+    }
+}
