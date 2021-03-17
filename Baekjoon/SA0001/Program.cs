9bef50da937b5e7b5079204e93008d7b7fe3b9a7
choiho <tizkzks96@naver.com>
2021-03-17 10:10:16 +0900

    스페셜 문제

---
+++ Baekjoon/SA0001/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SA0001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+
+    public class Solution
+    {
+        public void Run()
+        {
+            GetCase(int.Parse(Console.ReadLine()));
+        }
+
+        int[] GetCase(int caseCount)
+        {
+            int[] A = new int[caseCount];
+            for (int i = 0; i < caseCount; i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            return A;
+        }
+    }
+}
