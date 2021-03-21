b17496533a37fba180239c706793dc02ad79f4d9
choiho <tizkzks96@naver.com>
2021-03-22 00:29:40 +0900

    Solve - A15650 (백트래킹을 좀더 효율적으로 하는 방법 강구해야함)

---
+++ Baekjoon/A15650/A15650.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace A15650
+{
+    //N과 M(1)
+    class A15650
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Solution solution = new Solution();
+
+                solution.Run();
+            }
+        }
+
+        class Solution
+        {
+            StringBuilder sb = new StringBuilder();
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+
+            public void Run()
+            {
+                sw.AutoFlush = true;
+
+                var values = GetCaseValues();
+                List<int> remain = new List<int>();
+
+                for (int i = 1; i <= values.Key; i++)
+                {
+                    remain.Add(i);
+                }
+
+                BackTracking(values.Value, 0, remain, new List<int>());
+                sw.WriteLine(sb);
+            }
+
+            private void BackTracking(int targetDepth, int currentDepth, List<int> remain, List<int> list)
+            {
+                if (targetDepth == currentDepth)
+                {
+                    string value = "";
+                    value += $"{list[0]} ";
+                    for (int i = 1; i < list.Count; i++)
+                    {
+                        if (list[i - 1] < list[i])
+                            value += $"{list[i]} ";
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    sb.AppendLine(value);
+                    return;
+                }
+
+                for (int i = 0; i < remain.Count; i++)
+                {
+                    int addNum = remain[i];
+                    List<int> newList = new List<int>(list);
+                    newList.Add(addNum);
+                    List<int> newRemain = new List<int>(remain);
+                    newRemain.Remove(addNum);
+                    BackTracking(targetDepth, currentDepth + 1, newRemain, newList);
+                }
+
+                return;
+            }
+
+            private KeyValuePair<int, int> GetCaseValues()
+            {
+                string[] value = sr.ReadLine().Split(" ");
+
+                KeyValuePair<int, int> caseValues = new KeyValuePair<int, int>(int.Parse(value[0]), int.Parse(value[1]));
+
+                return caseValues;
+            }
+        }
+    }
+}
